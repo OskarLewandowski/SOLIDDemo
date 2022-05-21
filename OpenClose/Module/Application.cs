0dcd93a66ae3e263ac573a82e57171212f51d13b
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-21 15:03:48 +0200

    add files to module folder

---
+++ OpenClose/Module/Application.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClose.Module
+{
+    public class Application
+    {
+        public void Render(List<Shape> shapes)
+        {
+            for (int i = 0; i < shapes.Count; i++)
+            {
+                ShapeType type = shapes[i].ShapeType;
+
+                switch (type)
+                {
+                    case ShapeType.Circle:
+                        RenderCircle((Circle)shapes[i]);
+                        break;
+                    case ShapeType.Rectangle:
+                        RenderRectangle((Rectangle)shapes[i]);
+                        break;
+                }
+            }
+        }
+
+        private void RenderCircle(Circle circle)
+        {
+            Console.WriteLine("Render circle...");
+        }
+
+        private void RenderRectangle(Rectangle circle)
+        {
+            Console.WriteLine("Render rectangle...");
+        }
+
+    }
+}
---
+++ OpenClose/Module/ShapeType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClose.Module
+{
+    public enum ShapeType
+    {
+        Circle,
+        Rectangle
+    }
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Shape
+    {
+        public ShapeType ShapeType { get; set; }
+    }
+
+    public class Circle : Shape
+    {
+        public int Radius { get; set; }
+        public Point Center { get; set; }
+    }
+
+    public class Rectangle : Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Point TopLeft { get; set; }
+    }
+}
