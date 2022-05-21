8ca2d35a2dc02eb066bffd4c720d64f52b83778d
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-21 12:34:37 +0200

    Add project files.

---
+++ SOLIDDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SOLIDDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SOLID:");
+            Console.WriteLine("Single Responsibility Principle");
+            Console.WriteLine("Open Close Principle");
+            Console.WriteLine("Liskov Substitution Principle");
+            Console.WriteLine("Interface Segregation Principle");
+            Console.WriteLine("Dependency Inversion Principle");
+
+            Console.ReadLine();
+        }
+    }
+}
