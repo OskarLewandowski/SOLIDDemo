4dda3beefcd7f730fcdff32cd0cb4863cb79c54e
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-21 18:43:40 +0200

    example of bad DI

---
+++ DependencyInversion/AuthenticationManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    public class AuthenticationManager
+    {
+        public void Authentication(User user, string email, string password)
+        {
+            if (user.Email == email && user.Password == password)
+            {
+                EmailNotyfication emailNotyfication = new EmailNotyfication();
+                emailNotyfication.SendNotyfication(user);
+            }
+        }
+    }
+}
---
+++ DependencyInversion/EmailNotyfication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    public class EmailNotyfication
+    {
+        public void SendNotyfication(User user)
+        {
+            Console.WriteLine($"Sending notification to user: {user.Name}");
+        }
+    }
+}
