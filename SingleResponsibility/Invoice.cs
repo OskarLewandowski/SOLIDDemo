355722c28c58c9f5b117b4af0295815727a2ecda
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-21 12:49:44 +0200

    add files to SR

---
+++ SingleResponsibility/Invoice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    public class Invoice
+    {
+        public IEnumerable<LineItem> LineItems { get; set; }
+        public string Vendor { get; set; }
+        public string Vendee { get; set; }
+        public float Total { get; set; }
+
+        public Invoice(IEnumerable<LineItem> lineItems, string vendor, string vendee)
+        {
+            LineItems = lineItems;
+            Vendor = vendor;
+            Vendee = vendee;
+            Total = this.CalculateToatal();
+        }
+
+        public float CalculateToatal()
+        {
+            float total = 0;
+
+            foreach (var lineItem in LineItems)
+            {
+                total += lineItem.Price * lineItem.Count * (1 + lineItem.TaxRate);
+            }
+
+            return total;
+        }
+    }
+}
